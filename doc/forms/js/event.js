71954111d82aff6e604ff2e048704fc28e6a278d
Jeff Vroom <jeff@jvroom.com>
2021-01-08 19:07:14 -0800

    forms server uses the user/content layers part of siteBuilder
    replaced old UserSession tracking with the version based on
    the user layers in the siteBuilder. Renamed PageLayout to DocPageLayout
    avoid a conflict between the two class names (even though ultimately
    we are not including the html/core parts of the site builder where PageLayout
    is defined)
    
    added event tracking JSON api request handler so that we can track
    events from any page and added that to the doc Head tag

---
+++ doc/forms/js/event.js
@@ -0,0 +1,65 @@
+var sc_sid = null;
+var sc_wid = null;
+var sc_scrollDepth = 0;
+var sc_maxScrollDepth = 0;
+var sc_se = null;
+
+function sc_updateScrollDepth() {
+   var sh = sc_se.scrollHeight;
+   var wh = sc_se.clientHeight;
+   var st = sc_se.scrollTop;
+   sc_scrollDepth = Math.floor(100 * (st + wh) / sh);
+   if (sc_scrollDepth > 100)
+      sc_scrollDepth = 100;
+   if (sc_scrollDepth > sc_maxScrollDepth)
+      sc_maxScrollDepth = sc_scrollDepth;
+}
+
+window.addEventListener("load", function() {
+   sc_se = document.scrollingElement;
+   if (!sc_se)
+     sc_se = document.body;
+   sc_updateScrollDepth();
+
+   document.addEventListener("scroll", sc_updateScrollDepth);
+   window.addEventListener("resize", sc_updateScrollDepth);
+
+   var initReq = new XMLHttpRequest();
+   initReq.open("POST", "/form/event", true);
+   initReq.setRequestHeader("Content-type", "application/json");
+   initReq.onload = function(evt) {
+      var stat = initReq.status;
+      if (stat == 200) {
+         var resp = JSON.parse(initReq.responseText);
+         sc_sid = resp.sid;
+         sc_wid = resp.wid;
+      }
+      else {
+         console.error("Event tracking request returned status: " + stat);
+      }
+   };
+   initReq.onreadystatechange = function (evt) {
+      if (initReq.readyState == 4) {
+         var stat = initReq.status;
+         if (stat != 200)
+            console.error("Error response from tracking event request");
+      }
+   };
+   initReq.send(JSON.stringify({
+         u:window.location.pathname,
+         sw:window.screen.width,
+         sd:sc_maxScrollDepth,
+         sh:window.screen.height
+      }, null, 3));
+});
+
+
+window.addEventListener("unload", function() {
+   if (sc_sid) {
+      navigator.sendBeacon("/form/event", JSON.stringify({
+         sid:sc_sid,
+         wid:sc_wid,
+         sd:sc_maxScrollDepth,
+         close:true}));
+   }
+});
